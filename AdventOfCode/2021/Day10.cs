e52baa8c7e5771120c3ec209db661f52e79bd433
David Francis <iamdavidfrancis@gmail.com>
2021-12-10 23:36:04 -0800

    Add Day 10

---
+++ AdventOfCode/2021/Day10.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Statistics;
+
+namespace AdventOfCode._2021
+{
+    internal class Day10 : IAsyncAdventOfCodeProblem
+    {
+        private readonly char[] ClosingChars = new char[] { ')', ']', '}', '>' };
+        public async Task RunProblemAsync()
+        {
+            List<List<int>> heightMap = new List<List<int>>();
+
+            using (TextReader reader = File.OpenText("./2021/Day10Input.txt"))
+            {
+                int sum = 0;
+                List<ulong> lineScores = new();
+
+                string? line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    // Part 1
+                    // sum += ProcessLine(line).corruptionScore;
+
+                    var (isCorrupted, expected, _) = ProcessLine(line);
+
+                    // Part 2
+                    if (isCorrupted)
+                    {
+                        continue;
+                    }
+
+                    ulong lineScore = 0;
+
+                    while (expected.Any())
+                    {
+                        lineScore *= 5;
+
+                        switch (expected.Pop())
+                        {
+                            case ')':
+                                lineScore += 1;
+                                break;
+                            case ']':
+                                lineScore += 2;
+                                break;
+                            case '}':
+                                lineScore += 3;
+                                break;
+                            default:
+                                lineScore += 4;
+                                break;
+                        }
+                    }
+
+                    lineScores.Add(lineScore);
+                }
+
+                lineScores.Sort();
+
+                var middle = lineScores.Count / 2;
+
+                Console.WriteLine(lineScores[middle]);
+            }
+        }
+
+        private (bool isCorrupted, Stack<char> expected, int corruptionScore) ProcessLine(string line)
+        {
+            Stack<char> expected = new();
+
+            int idx = 0;
+            foreach (char c in line)
+            {
+                if (IsClosing(c))
+                {
+                    if (!expected.TryPop(out char expectedChar) || c != expectedChar)
+                    {
+                        // Console.WriteLine($"{line}\tExpected {expectedChar}, but found {c} at position {idx}");
+                        return (true, expected, ScoreCorruption(c));
+                    }
+                }
+                else
+                {
+                    switch (c)
+                    {
+                        case '(':
+                            expected.Push(')');
+                            break;
+                        case '[':
+                            expected.Push(']');
+                            break;
+                        case '{':
+                            expected.Push('}');
+                            break;
+                        default:
+                            expected.Push('>');
+                            break;
+                    }
+                }
+
+                idx++;
+            }
+
+            return (false, expected, 0);
+        }
+
+        private bool IsClosing(char input)
+        {
+            return ClosingChars.Contains(input);
+        }
+
+        private int ScoreCorruption(char input)
+        {
+            switch (input)
+            {
+                case ')':
+                    return 3;
+                case ']':
+                    return 57;
+                case '}':
+                    return 1197;
+                default:
+                    return 25137;
+            }
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using AdventOfCode;
 
-var problem = new AdventOfCode._2021.Day11();
+var problem = new AdventOfCode._2021.Day10();
 
 if (problem is IAdventOfCodeProblem syncProblem)
 {
