8d537d47f98d74aacd74e759f08e2bc3d0ac7e0a
David Francis <iamdavidfrancis@gmail.com>
2021-12-11 20:05:19 -0800

    Add 2015 day 1

---
+++ AdventOfCode/2015/Day01.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015
+{
+    internal class Day01 : IAsyncAdventOfCodeProblem
+    {
+        public async Task RunProblemAsync()
+        {
+            using (TextReader reader = File.OpenText("./2015/Day01Input.txt"))
+            {
+                int currentFloor = 0;
+
+                string? line = await reader.ReadLineAsync();
+
+                if (line == null)
+                {
+                    return;
+                }
+
+                var idx = 1;
+                foreach (var instruction in line)
+                {
+                    if (instruction == '(')
+                    {
+                        currentFloor++;
+                    }
+                    else
+                    {
+                        currentFloor--;
+                    }
+
+                    // Part 2 only
+                    if (currentFloor == -1)
+                    {
+                        break;
+                    }
+
+                    idx++;
+                }
+
+                // Part 1
+                // Console.WriteLine(currentFloor);
+                
+                // Part 2
+                Console.WriteLine(idx);
+            }
+        }
+    }
+}
