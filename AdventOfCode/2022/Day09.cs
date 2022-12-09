b4d8997c5b5afb3583b3afd633faed8ebaee17e8
David Francis <iamdavidfrancis@gmail.com>
2022-12-09 09:26:09 -0800

    Add 2022 Day 9

---
+++ AdventOfCode/2022/Day09.cs
@@ -0,0 +1,108 @@
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2022
+{
+    internal class Day09 : IAsyncAdventOfCodeProblem
+    {
+        // Note: For this, I pre-processed the input slightly.
+        public async Task RunProblemAsync()
+        {
+            using (TextReader reader = File.OpenText("./2022/Day09.txt"))
+            {
+                string? line;
+
+                List<Point2D> knots1 = Enumerable.Range(1, 2).Select(c => new Point2D(0, 0)).ToList();
+                List<Point2D> knots2 = Enumerable.Range(1, 10).Select(c => new Point2D(0, 0)).ToList();
+                
+                HashSet<Point2D> visitedPoints1 = new HashSet<Point2D>();
+                HashSet<Point2D> visitedPoints2 = new HashSet<Point2D>();
+
+                visitedPoints1.Add(new Point2D(0,0));
+                visitedPoints2.Add(new Point2D(0,0));
+                
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    var pieces = line.Split(" ");
+                    var direction = pieces[0];
+                    var distance = Int32.Parse(pieces[1]);
+                    
+                    for (int i = 0; i < distance; ++i)
+                    {
+                        switch (direction) {
+                            case "U":
+                                knots1[0] = knots1[0].Up();
+                                knots2[0] = knots2[0].Up();
+                                break;
+                            case "D":
+                                knots1[0] = knots1[0].Down();
+                                knots2[0] = knots2[0].Down();
+                                break;
+                            case "L":
+                                knots1[0] = knots1[0].Left();
+                                knots2[0] = knots2[0].Left();
+                                break;
+                            default:
+                                knots1[0] = knots1[0].Right();
+                                knots2[0] = knots2[0].Right();
+                                break;
+                        }
+
+                        for (int j = 0; j < knots1.Count - 1; j++)
+                        {
+                            ProcessKnotPair(knots1, j, j+1);
+                        }
+
+                        for (int j = 0; j < knots2.Count - 1; j++)
+                        {
+                            ProcessKnotPair(knots2, j, j+1);
+                        }
+
+                        
+                        if(!visitedPoints1.Contains(knots1.Last()))
+                        {
+                            visitedPoints1.Add(knots1.Last());
+                        }
+
+                        if(!visitedPoints2.Contains(knots2.Last()))
+                        {
+                            visitedPoints2.Add(knots2.Last());
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Part 1: {visitedPoints1.Count}");
+                Console.WriteLine($"Part 2: {visitedPoints2.Count}");
+            }
+        }
+
+        private void ProcessKnotPair(List<Point2D> knots, int idxA, int idxB)
+        {
+            var delta = knots[idxA] - knots[idxB];
+            if (Math.Abs(delta.X) == 2 && delta.Y == 0)
+            {
+                knots[idxB] = knots[idxB].Right(delta.X/2);
+            }
+            else if (Math.Abs(delta.X) == 2 && Math.Abs(delta.Y) == 1)
+            {
+                knots[idxB] = knots[idxB].Right(delta.X/2);
+                knots[idxB] = knots[idxB].Up(delta.Y);
+            }
+            else if (Math.Abs(delta.X) == 0 && Math.Abs(delta.Y) == 2)
+            {
+                knots[idxB] = knots[idxB].Up(delta.Y/2);
+            }
+            else if (Math.Abs(delta.X) == 1 && Math.Abs(delta.Y) == 2)
+            {
+                knots[idxB] = knots[idxB].Right(delta.X);
+                knots[idxB] = knots[idxB].Up(delta.Y/2);
+            }
+            else if (Math.Abs(delta.X) == 2 && Math.Abs(delta.Y) == 2)
+            {
+                knots[idxB] = knots[idxB].Right(delta.X/2);
+                knots[idxB] = knots[idxB].Up(delta.Y/2);
+            }
+
+        }
+    }
+}
+
--- AdventOfCode/Point2D.cs
+++ AdventOfCode/Point2D.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
-    public record Point2D(int X, int Y);
+    public record Point2D(int X, int Y) {
+        public static Point2D operator +(Point2D a, Point2D b) => new Point2D(a.X + b.X, a.Y + b.Y);
+        public static Point2D operator -(Point2D a, Point2D b) => new Point2D(a.X - b.X, a.Y - b.Y);
+    }
 
     public static class Point2DExtensions
     {
@@ -34,6 +37,22 @@ namespace AdventOfCode
                 new Point2D(source.X + 1, source.Y - 1),
                 new Point2D(source.X - 1, source.Y + 1),
             };
+        }
+
+        public static Point2D Up(this Point2D source, int distance = 1) {
+            return source with { Y = source.Y + distance };
+        }
+        
+        public static Point2D Down(this Point2D source, int distance = 1) {
+            return source with { Y = source.Y - distance };
+        }
+        
+        public static Point2D Left(this Point2D source, int distance = 1) {
+            return source with { X = source.X - distance };
+        }
+        
+        public static Point2D Right(this Point2D source, int distance = 1) {
+            return source with { X = source.X + distance };
         }
     }
 }
