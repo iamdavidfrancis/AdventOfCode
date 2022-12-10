2470f8578467e71fac7f37274ee1fba92efa1380
David Francis <iamdavidfrancis@gmail.com>
2022-12-10 00:07:43 -0800

    Formatting

--- AdventOfCode/2022/Day10.cs
+++ AdventOfCode/2022/Day10.cs
@@ -54,7 +54,6 @@ namespace AdventOfCode._2022
                         Console.Write(".");
                     }
 
-
                     if (pos == 39) {
                         Console.WriteLine();
                     }
