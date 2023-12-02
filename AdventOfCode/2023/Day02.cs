c371bc31f43c6bb819f4d941a972bf08c5c82bcb
David Francis <iamdavidfrancis@gmail.com>
2023-12-02 09:50:03 -0800

    Misc changes

---
+++ AdventOfCode/2023/Day02.cs
@@ -0,0 +1,26 @@
+namespace AdventOfCode._2023;
+
+public class Day02 : IAsyncAdventOfCodeProblem
+{
+
+    public async Task RunProblemAsync()
+    {
+        using (TextReader reader = File.OpenText("./2023/Day02.txt"))
+        {
+            string? line;
+            
+            while ((line = await reader.ReadLineAsync()) != null) 
+            {
+                
+            }
+
+            Console.WriteLine("Part 1");
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Part 2");
+            Console.WriteLine();
+        }
+    }
+}
